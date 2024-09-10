527bfffea40f1d418e8cac5cc7c5ff0bd994b46d
JC <juancloz@gmail.com>
2024-09-10 14:34:03 -0600

    Pretty much a new script.

--- yyc.R
+++ yyc.R
@@ -2,99 +2,93 @@
 # library(remotes)
 # remotes::install_github("ropensci/osmdata") # this approach retrieves the latests version of osmdata
 
-library(tidyverse)
-library(osmdata) # package for working with streets
-library(showtext) # for custom fonts
-library(ggmap)
-library(rvest)
-library(svglite)
+# Load libraries
+library(tidyverse) # Clean, manipulate, and visualize data efficiently.
+library(osmdata) # Extract geographic data from.
+library(showtext) # To customize fonts
+library(ggmap) # Create maps with geographic data.
+library(rvest) # Scrape web pages for data.
+library(sf) # To work  with simple features (geometry, attributes).
+library(raster) # Handle raster data (images, grids).
+library(lwgeom) # Perform advanced geometry operations.
 
-# Set the bb for the desired city
-yyc <- getbb('Calgary Canada')
+# Define the place the map
+place <- 'Calgary Canada'
 
-big_streets <- yyc %>%
-  opq()%>%
+# Get bbbox
+placebb <- getbb(place)
+
+# Get the featurues to be mapped
+big_streets <- placebb %>% 
+  opq() %>%
   add_osm_feature(key = 'highway', 
                   value = c('motorway', 'primary', 'motorway_link', 'primary_link')) %>%
   osmdata_sf()
 
-med_streets <- yyc%>%
-  opq()%>%
-  add_osm_feature(key = "highway", 
-                  value = c("secondary", "tertiary", "secondary_link", "tertiary_link")) %>%
+med_streets <- placebb %>%
+  opq() %>%
+  add_osm_feature(key = 'highway', 
+                  value = c('secondary', 'tertiary', 'secondary_link', 'tertiary_link')) %>%
   osmdata_sf()
 
-
-small_streets <- yyc%>%
-  opq()%>%
-  add_osm_feature(key = "highway", 
-                  value = c("residential", "living_street",
-                           "footway"
-                  )) %>%
+small_streets <- placebb %>% 
+  opq() %>%
+  add_osm_feature(key = 'highway', 
+                  value = c('residential', 'living_street', 'unclassified', 'footway')) %>%
   osmdata_sf()
 
-bike <- yyc%>%
-  opq()%>%
-  add_osm_feature(key = "highway", 
-                  value = c('cycleway', 'path'
-                  )) %>%
-  osmdata_sf()
+# Municipal boundaries
+all_boundaries <- opq(place) %>%
+  add_osm_feature(key = 'boundary', 
+                  value = c('administrative')) %>%
+  osmdata_sf() %>% 
+  unname_osmdata_sf() %>% 
+  .$osm_multipolygons
 
-river <- yyc%>%
-  opq()%>%
-  add_osm_feature(key = "waterway", value = "river") %>%
-  osmdata_sf()
+# Correct boundaries
+boundary <- all_boundaries %>% 
+  filter(osm_id == 3227127) %>% # This is the specific osm_id for the city of Calgary
+  dplyr::select()
 
-railway <- yyc%>%
-  opq()%>%
-  add_osm_feature(key = "railway", value="rail") %>%
-  osmdata_sf()
+# Extract the lines from the streets objects
+big_streets_lines <- big_streets$osm_lines
+med_streets_lines <- med_streets$osm_lines
+small_streets_lines <- small_streets$osm_lines
 
-yyc_plot <- ggplot() +
-  geom_sf(data = river$osm_lines,
+# Perform the intersection with the boundary
+big_streets_cropped <- st_intersection(big_streets_lines, boundary)
+med_streets_cropped <- st_intersection(med_streets_lines, boundary)
+small_streets_cropped <- st_intersection(small_streets_lines, boundary)
+
+# Use st_bbox to get the bounding box from the boundary
+bounding_box <- st_bbox(boundary)
+
+# Boundary-shaped map
+yyc <- ggplot() +
+  geom_sf(data = med_streets_cropped,
           inherit.aes = FALSE,
-          color = "steelblue",
-          size = 15,
-          alpha = 1) +
-  # geom_sf(data = bike$osm_lines,
-  #         inherit.aes = FALSE,
-  #         color = 'green',
-  #         size = .3,
-  #         alpha = 1)+
-  geom_sf(data = railway$osm_lines,
-          inherit.aes = FALSE,
-          color = "black",
-          size = .2,
-          linetype="dotdash",
-          alpha = .5) +
-  geom_sf(data = med_streets$osm_lines,
-          inherit.aes = FALSE,
-          color = "black",
+          color = '#999999',
           size = .3,
           alpha = .5) +
-  geom_sf(data = small_streets$osm_lines,
+  geom_sf(data = small_streets_cropped,
           inherit.aes = FALSE,
-          color = "#666666",
+          color = '#D9D9D9',
           size = .2,
           alpha = .3) +
-  geom_sf(data = big_streets$osm_lines,
+  geom_sf(data = big_streets_cropped,
           inherit.aes = FALSE,
-          color = "black",
-          size = .5,
+          color = '#222222',
+          size = .4,
           alpha = .6) +
-    coord_sf(xlim = c(-114.35, -113.8),
-           ylim = c(51.25, 50.8),
-           expand = FALSE)+
-  theme_void() + # get rid of background color, grid lines, etc.
-  theme(plot.title = element_text(size = 80, family = "helvetica", face="bold", hjust=.5),
-        plot.subtitle = element_text(family = "helvetica", size = 20, hjust=.5, margin=margin(2, 0, 5, 0))) +
-  labs(title = "CALGARY", subtitle = "51.050°N / 114.085°W")
-
-# Save the plot as SVG for futher editing
-ggsave("yyc_plot.svg", plot = yyc_plot, device = "svg", width = 18, height = 24, units = "in")  
-
-# # Save the plot as a PDF file with a specified width and height
-# ggsave("yyc_plot.pdf", plot = yyc_plot, device = "pdf", width = 18, height = 24, units = "in")
-# 
-# # Save the plot as a PNG file with a specified width and height
-# ggsave("yyc_plot.png", plot = yyc_plot, device = "png", width = 6, height = 8, units = "in")
+  theme_void() +
+  theme(plot.title = element_text(size = 20,
+                                  family = 'Spline Sans',
+                                  hjust = 0,
+                                  color = '#222222'),
+        plot.subtitle = element_text(size = 8,
+                                     family = 'Spline Sans Mono',
+                                     hjust = 0,
+                                     color = '#222222',
+                                     margin=margin(2, 0, 5, 0))) +
+  labs(title = 'Calgary',
+       subtitle = '51.056°N / 114.068°W')
